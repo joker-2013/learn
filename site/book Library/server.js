a61954b61a346271290bd2a8b31fca46688e814a
joker-2013 <yyjpig@163.com>
2013-08-06 16:48:53 +0800

    book  library

---
+++ site/book Library/server.js
@@ -0,0 +1,23 @@
+var _root=__dirname,
+    express=require('express'),
+    path=require('path'),
+    mongoose=require('mongoose'),
+    port=4711;
+console.log(_root)
+var app=express();
+app.configure(function(){
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(_root,'public')));
+	app.use(express.errorHandler({
+		dumpExceptions:true,
+		showStack:true
+	}))
+});
+app.listen(port,function(){
+	console.log('Express server listening on port %d in %s mode',port,app.settings.env)
+});
+
+
+
